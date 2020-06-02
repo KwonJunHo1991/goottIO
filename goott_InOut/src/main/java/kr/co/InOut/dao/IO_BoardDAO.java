6a66c56d1b1e457499af38098fd4c3abaffe499c
KwonJunHo1991 <ovover5@gmail.com>
2020-06-02 16:19:52 +0900

    DTO 더 추가하고. mapper 폴더 만들고 이동. rootContext 수정.

---
+++ goott_InOut/src/main/java/kr/co/InOut/dao/IO_BoardDAO.java
@@ -0,0 +1,24 @@
+package kr.co.InOut.dao;
+
+import java.util.ArrayList;
+
+import kr.co.InOut.dto.IO_BoardDTO;
+
+public class IO_BoardDAO {
+	
+	//전체 조회.(return list)
+	public ArrayList<IO_BoardDTO> selectAllBoard(){
+		
+		//DB에서 받아오기.
+		
+		
+		//ArrayList<IO_BoardDTO> list = 
+		
+		//return list;
+		return null;
+		
+	}
+	//총 게시글수 조회.(count)
+	
+	
+}
