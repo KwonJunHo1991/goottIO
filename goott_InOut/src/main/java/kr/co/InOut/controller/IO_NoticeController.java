b41d454773fed7590519f5dc92a991496a026b54
lhy528 <sin_lhy@naver.com>
2020-06-08 15:47:44 +0900

    채용공고를 4단계로 나누기 진행중

--- goott_InOut/src/main/java/kr/co/InOut/controller/IO_NoticeController.java
+++ goott_InOut/src/main/java/kr/co/InOut/controller/IO_NoticeController.java
@@ -3,10 +3,15 @@ package kr.co.InOut.controller;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.co.InOut.dao.IO_NoticeDAO;
+import kr.co.InOut.dto.IO_NoticeDTO;
+import kr.co.InOut.dto.IO_Notice_PrcsDTO;
+import kr.co.InOut.dto.IO_Notice_QnaDTO;
+import kr.co.InOut.dto.IO_Notice_WantDTO;
 
 @Controller
 public class IO_NoticeController {
@@ -16,10 +21,38 @@ public class IO_NoticeController {
 		this.dao = dao;
 	}
 	
+	// 새 공고 등록하는 창 들어가기
 	@RequestMapping(value = "/company/newNotice.do", method = RequestMethod.GET)
 	public String showNoticeWriter() {
 		
 		return "newNotice";
 	}
+	
+	// 새 공고 등록작업(기본정보 DB에 넣고 모집분야 입력하는 창으로)
+	@RequestMapping(value = "/company/newNoticeStep1.do", method = RequestMethod.POST)
+	public String insertNotice(@ModelAttribute()IO_NoticeDTO dto) {
+		dao.insertOneNotice(dto);
+		return "newNoticeStep1";
+	}
+	// 새 공고 등록작업(모집분야 DB에 넣고 채용절차 입력하는 창으로)
+	@RequestMapping(value = "/company/newNoticeStep2.do", method = RequestMethod.POST)
+	public String insertNotice2(@ModelAttribute()IO_Notice_WantDTO dto2) {
+		dao.insertOneNoticeWant(dto2);
+		return "newNoticeStep2";
+	}
+	// 새 공고 등록작업(채용절차 DB에 넣고 인사통 입력하는 창으로)
+	@RequestMapping(value = "/company/newNoticeStep3.do", method = RequestMethod.POST)
+	public String insertNotice3(@ModelAttribute()IO_Notice_PrcsDTO dto3) {
+		dao.insertOneNoticePrcs(dto3);
+		return "newNoticeStep3";
+	}
+	// 인사통 DB에 넣고 공고등록 완료창으로
+	@RequestMapping(value = "/company/newNoticeOk.do", method = RequestMethod.POST)
+	public String insertNotice4(@ModelAttribute()IO_Notice_QnaDTO dto4) {
+		dao.insertOneNoticeQna(dto4);
+		return "newNoticeOk";
+	}
+	
+	
 	
 }
--- goott_InOut/src/main/java/kr/co/InOut/dao/IO_NoticeDAO.java
+++ goott_InOut/src/main/java/kr/co/InOut/dao/IO_NoticeDAO.java
@@ -6,6 +6,9 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Component;
 
 import kr.co.InOut.dto.IO_NoticeDTO;
+import kr.co.InOut.dto.IO_Notice_PrcsDTO;
+import kr.co.InOut.dto.IO_Notice_QnaDTO;
+import kr.co.InOut.dto.IO_Notice_WantDTO;
 
 @Component
 public class IO_NoticeDAO {
@@ -21,13 +24,23 @@ public class IO_NoticeDAO {
 	public void insertOneNotice(IO_NoticeDTO dto) {
 		ss.insert("insertOneNotice", dto);
 	}
+	public void insertOneNoticeWant(IO_Notice_WantDTO dto) {
+		ss.insert("insertOneNoticeWant", dto);
+	}
+	public void insertOneNoticeQna(IO_Notice_QnaDTO dto) {
+		ss.insert("insertOneNoticeQna", dto);
+	}
+	public void insertOneNoticePrcs(IO_Notice_PrcsDTO dto) {
+		ss.insert("insertOneNoticePrcs", dto);
+	}
 	
-	public IO_NoticeDTO selectOneNoticeByNn(IO_NoticeDTO dto) {
+	
+	public IO_NoticeDTO selectOneNoticeByCnNn(IO_NoticeDTO dto) {
 		return ss.selectOne("selectOneNoticeByCnNn", dto);
 	}
 	
-	public void updateOneNotice(IO_NoticeDTO dto) {
-		ss.update("updateOneNoticeByCn", dto);
+	public void updateOneNoticeByCnNn(IO_NoticeDTO dto) {
+		ss.update("updateOneNoticeByCnNn", dto);
 	}
 }
 
