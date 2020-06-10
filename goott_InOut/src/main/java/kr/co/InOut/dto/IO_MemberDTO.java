1a14744480ca2b7de871c4232d189411307d56c7
lhy528 <sin_lhy@naver.com>
2020-06-10 19:34:48 +0900

    dto 변경

--- goott_InOut/src/main/java/kr/co/InOut/dto/IO_MemberDTO.java
+++ goott_InOut/src/main/java/kr/co/InOut/dto/IO_MemberDTO.java
@@ -16,8 +16,11 @@ public class IO_MemberDTO {
 	
 	public IO_MemberDTO() {}
 
-	public IO_MemberDTO(String mem_id, String mem_pw, String mem_name, int mem_birth, String mem_email, int mem_phone,
-			String mem_add, String mem_add1, String mem_image, String mem_anno, int mem_sex) {
+
+	public IO_MemberDTO(String mem_id, String mem_pw, String mem_name,
+			Integer mem_birth, String mem_email, Integer mem_phone,
+			String mem_add, String mem_add1, String mem_image, String mem_anno,
+			Integer mem_sex) {
 		super();
 		this.mem_id = mem_id;
 		this.mem_pw = mem_pw;
@@ -32,93 +35,116 @@ public class IO_MemberDTO {
 		this.mem_sex = mem_sex;
 	}
 
+
 	public String getMem_id() {
 		return mem_id;
 	}
+
 
 	public void setMem_id(String mem_id) {
 		this.mem_id = mem_id;
 	}
 
+
 	public String getMem_pw() {
 		return mem_pw;
 	}
+
 
 	public void setMem_pw(String mem_pw) {
 		this.mem_pw = mem_pw;
 	}
 
+
 	public String getMem_name() {
 		return mem_name;
 	}
+
 
 	public void setMem_name(String mem_name) {
 		this.mem_name = mem_name;
 	}
 
-	public int getMem_birth() {
+
+	public Integer getMem_birth() {
 		return mem_birth;
 	}
 
-	public void setMem_birth(int mem_birth) {
+
+	public void setMem_birth(Integer mem_birth) {
 		this.mem_birth = mem_birth;
 	}
+
 
 	public String getMem_email() {
 		return mem_email;
 	}
 
+
 	public void setMem_email(String mem_email) {
 		this.mem_email = mem_email;
 	}
 
-	public int getMem_phone() {
+
+	public Integer getMem_phone() {
 		return mem_phone;
 	}
 
-	public void setMem_phone(int mem_phone) {
+
+	public void setMem_phone(Integer mem_phone) {
 		this.mem_phone = mem_phone;
 	}
+
 
 	public String getMem_add() {
 		return mem_add;
 	}
 
+
 	public void setMem_add(String mem_add) {
 		this.mem_add = mem_add;
 	}
+
 
 	public String getMem_add1() {
 		return mem_add1;
 	}
 
+
 	public void setMem_add1(String mem_add1) {
 		this.mem_add1 = mem_add1;
 	}
+
 
 	public String getMem_image() {
 		return mem_image;
 	}
 
+
 	public void setMem_image(String mem_image) {
 		this.mem_image = mem_image;
 	}
+
 
 	public String getMem_anno() {
 		return mem_anno;
 	}
 
+
 	public void setMem_anno(String mem_anno) {
 		this.mem_anno = mem_anno;
 	}
 
-	public int getMem_sex() {
+
+	public Integer getMem_sex() {
 		return mem_sex;
 	}
 
-	public void setMem_sex(int mem_sex) {
+
+	public void setMem_sex(Integer mem_sex) {
 		this.mem_sex = mem_sex;
 	}
+
 
 	@Override
 	public String toString() {
--- goott_InOut/src/main/java/kr/co/InOut/dto/IO_Notice_QnaDTO.java
+++ goott_InOut/src/main/java/kr/co/InOut/dto/IO_Notice_QnaDTO.java
@@ -6,32 +6,22 @@ public class IO_Notice_QnaDTO {
 	public IO_Notice_QnaDTO() {
 		// TODO Auto-generated constructor stub
 	}
-	public IO_Notice_QnaDTO(int notice_qna_num, int notice_num, int comp_num,
-			String comp_id, String notice_qna_q, String notice_qna_a) {
-		super();
-		this.notice_qna_num = notice_qna_num;
-		this.notice_num = notice_num;
-		this.comp_num = comp_num;
-		this.comp_id = comp_id;
-		this.notice_qna_q = notice_qna_q;
-		this.notice_qna_a = notice_qna_a;
-	}
-	public int getNotice_qna_num() {
+	public Integer getNotice_qna_num() {
 		return notice_qna_num;
 	}
-	public void setNotice_qna_num(int notice_qna_num) {
+	public void setNotice_qna_num(Integer notice_qna_num) {
 		this.notice_qna_num = notice_qna_num;
 	}
-	public int getNotice_num() {
+	public Integer getNotice_num() {
 		return notice_num;
 	}
-	public void setNotice_num(int notice_num) {
+	public void setNotice_num(Integer notice_num) {
 		this.notice_num = notice_num;
 	}
-	public int getComp_num() {
+	public Integer getComp_num() {
 		return comp_num;
 	}
-	public void setComp_num(int comp_num) {
+	public void setComp_num(Integer comp_num) {
 		this.comp_num = comp_num;
 	}
 	public String getComp_id() {
@@ -50,6 +40,17 @@ public class IO_Notice_QnaDTO {
 		return notice_qna_a;
 	}
 	public void setNotice_qna_a(String notice_qna_a) {
+		this.notice_qna_a = notice_qna_a;
+	}
+	public IO_Notice_QnaDTO(Integer notice_qna_num, Integer notice_num,
+			Integer comp_num, String comp_id, String notice_qna_q,
+			String notice_qna_a) {
+		super();
+		this.notice_qna_num = notice_qna_num;
+		this.notice_num = notice_num;
+		this.comp_num = comp_num;
+		this.comp_id = comp_id;
+		this.notice_qna_q = notice_qna_q;
 		this.notice_qna_a = notice_qna_a;
 	}
 }
